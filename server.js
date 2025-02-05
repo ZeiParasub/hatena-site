d23b5f32f006a8aabba32cf2ff0200dd5ebcbd36
ZeiParasub <ZeiPara@outlook.jp>
2025-02-05 19:22:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.get('/image/officialstudio', (req, res) => {
 app.use('/register', express.static(path.join(__dirname, 'register')));
 app.use('/login', express.static(path.join(__dirname, 'login')));
 app.use('/menu', express.static(path.join(__dirname, 'menu')));
+app.use('/scratch-auth', express.static(path.join(__dirname, 'scratch-auth')));
 
 // GETリクエストの処理
 app.get('/api/test', (req, res) => {
